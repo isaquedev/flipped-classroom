d6fe77e1096f437ac1c63adb2dc6b81ea6898031
Isaque Dev <isaquehernandes@gmail.com>
2018-11-07 06:03:43 -0200

    login

--- front/src/templates/login.vue
+++ front/src/templates/login.vue
@@ -69,8 +69,6 @@ export default {
             this.$store.dispatch('auth/login', this.data).catch(() => {
                 this.error = true;
                 this.$refs.form.reset();
-            }).then(() => {
-                window.location = '/';
             });
         }
     }
