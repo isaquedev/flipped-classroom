f869d0b5baabe788c448cc6f6feeb217fb6ab037
Isaque Dev <isaquehernandes@gmail.com>
2018-11-07 06:01:04 -0200

    db

--- app/config/containers.php
+++ app/config/containers.php
@@ -7,10 +7,11 @@ $container['events'] = function(){
 $container['settings'] = function() {
     return [
         'db' => [
-            'dsn' => 'mysql:host=localhost',
-            'database' => 'flipped_classroom_edu_system',
-            'username' => 'root',
-            'password' => '',
+            'dsn' => 'pgsql:host=ec2-184-73-169-151.compute-1.amazonaws.com',
+            'database' => 'dbbkuhe3g5onj1',
+            'username' => 'fvgxlekzbvteyd',
+            'port' => '5432',
+            'password' => '652feed25804ed3fbd3c5016995dd15893afaae94c569dafc96b1a328fd06f7d',
             'options' => [
                 \PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'
             ]
@@ -19,7 +20,7 @@ $container['settings'] = function() {
 };
 
 $container['db'] = function($c) {
-    $dsn = $c['settings']['db']['dsn'] . ';dbname=' . $c['settings']['db']['database'];
+    $dsn = $c['settings']['db']['dsn'] . ';port=' . $c['settings']['db']['port'] . ';dbname=' . $c['settings']['db']['database'];
     $username = $c['settings']['db']['username'];
     $password = $c['settings']['db']['password'];
     $options = $c['settings']['db']['options'];
