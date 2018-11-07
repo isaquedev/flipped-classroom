78cd272eb08e7c62e9f7b6334bf62a6842b82167
Isaque Dev <isaquehernandes@gmail.com>
2018-11-07 05:42:41 -0200

    module

--- config/modules.php
+++ config/modules.php
@@ -2,6 +2,6 @@
 
 $modules = [
     __DIR__ . '/../app/Module.php' => 'App\Module',
-    __DIR__ . '/../modules/SchoolClasses/Module.php' => 'UNI\Framework\SchoolClasses\Module',
-    __DIR__ . '/../modules/Questionnaires/Module.php' => 'UNI\Framework\Questionnaires\Module',
+    __DIR__ . '/../modules/schoolclasses/Module.php' => 'UNI\Framework\SchoolClasses\Module',
+    __DIR__ . '/../modules/questionnaires/Module.php' => 'UNI\Framework\Questionnaires\Module',
 ];
