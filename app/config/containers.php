d4648f563c3fab3f104b23071dacac45f8ae2676
Isaque Dev <isaquehernandes@gmail.com>
2018-11-07 08:38:10 -0200

    database resolve

--- app/config/containers.php
+++ app/config/containers.php
@@ -6,7 +6,7 @@ $container['events'] = function(){
 
 $container['settings'] = function() {
     return [
-        'db' => [
+        'online-db' => [
             'dsn' => 'pgsql:host=ec2-184-73-169-151.compute-1.amazonaws.com',
             'database' => 'dbbkuhe3g5onj1',
             'username' => 'fvgxlekzbvteyd',
@@ -15,15 +15,31 @@ $container['settings'] = function() {
             'options' => [
                 \PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'
             ]
+        ],
+        'local-db' => [
+            'dsn' => 'mysql:host=localhost',
+            'database' => 'flipped_classroom_edu_system',
+            'username' => 'root',
+            'port' => '',
+            'password' => '',
+            'options' => [
+                \PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'
+            ]
+        ], 
+        'db' => [
+            'online' => 'online-db',
+            'offline' => 'local-db'
         ]
     ];
 };
 
 $container['db'] = function($c) {
-    $dsn = $c['settings']['db']['dsn'] . ';port=' . $c['settings']['db']['port'] . ';dbname=' . $c['settings']['db']['database'];
-    $username = $c['settings']['db']['username'];
-    $password = $c['settings']['db']['password'];
-    $options = $c['settings']['db']['options'];
+    $isOnline = false;
+    $db = $isOnline == true ? $c['settings']['db']['online'] : $c['settings']['db']['offline'];
+    $dsn = $c['settings'][$db]['dsn'] . ';port=' . $c['settings'][$db]['port'] . ';dbname=' . $c['settings'][$db]['database'];
+    $username = $c['settings'][$db]['username'];
+    $password = $c['settings'][$db]['password'];
+    $options = $c['settings'][$db]['options'];
 
     $pdo = new \PDO($dsn, $username, $password, $options);
     
